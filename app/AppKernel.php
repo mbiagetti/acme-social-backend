2fad2d58d26b2a9f866f21d0c74f47a6c29cf427
Matteo Biagetti <m.biagetti@dnsee.com>
2015-04-13 15:37:54 +0200

    Add guzzle Add twitter client

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Idea\AdminBundle\IdeaAdminBundle(),
+            new Misd\GuzzleBundle\MisdGuzzleBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
